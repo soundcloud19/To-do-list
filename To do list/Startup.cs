589291d1dad7ec8943078299fa2176d178087365
soundcloud19 <57188292+soundcloud19@users.noreply.github.com>
2019-11-28 20:43:02 +0100

    Create the controller skeleton

--- To do list/Startup.cs
+++ To do list/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
 
 namespace To_do_list
 {
@@ -25,6 +27,8 @@ namespace To_do_list
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt =>
+               opt.UseInMemoryDatabase("TodoList"));
             services.AddControllers();
         }
 
